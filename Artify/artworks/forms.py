d3a97f75d78314336896f0603729c73619f32f14
Daniel <torta979@gmail.com>
2024-12-09 16:38:26 +0200

    artwork rework

--- Artify/artworks/forms.py
+++ Artify/artworks/forms.py
@@ -4,7 +4,7 @@ from .models import Artwork
 class ArtworkForm(forms.ModelForm):
     class Meta:
         model = Artwork
-        fields = ['title', 'image', 'description', 'price']
+        fields = ['artist', 'title', 'image', 'description', 'price']
         widgets = {
             'description': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Describe the artwork...'}),
         }
--- Artify/artworks/models.py
+++ Artify/artworks/models.py
@@ -1,17 +1,13 @@
 from django.db import models
-
-from Artify.categories.models import ArtCategory
-from Artify.profiles.models import ArtistProfile
+from Artify.profiles.models import Artist
 
 
 class Artwork(models.Model):
-    artist = models.ForeignKey(ArtistProfile, on_delete=models.CASCADE, related_name='artworks')  # Linked to an artist
+    artist = models.ForeignKey(Artist, on_delete=models.SET_NULL, null=True, blank=True)  # Optional artist
     title = models.CharField(max_length=100)  # Title of the artwork
     description = models.TextField(blank=True)  # Optional description
-    category = models.ForeignKey(ArtCategory, on_delete=models.SET_NULL, null=True)  # Category of the artwork
     image = models.ImageField(upload_to='artworks/')  # Artwork image
     price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)  # Price if it's for sale
-    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp for when it was uploaded
 
     def __str__(self):
         return f"{self.title} by {self.artist.user.username}"
